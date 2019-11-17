25314b4e40ec64d49d3cf1ee9cd14a38102c55a9
Seraphim <freedom1040425@gmail.com>
2019-11-17 19:52:03 +0800

    Last iiiAndroid 專題介面

--- app/src/main/java/com/example/iiiorgandroidlast/MainActivity.java
+++ app/src/main/java/com/example/iiiorgandroidlast/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.iiiorgandroidlast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ListView;
 
 import java.lang.reflect.Array;
@@ -16,5 +18,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
+
+    public void gotoMain(View view) {
+        Intent intent = new Intent(this,Page2Activity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/listitem.java
@@ -0,0 +1,15 @@
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.BundleCompat;
+
+import com.example.iiiorgandroidlast.R;
+
+public class listitem extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.listitem);
+    }
+
+}
